de05d888df5c3c1f84294560032b90a29576e499
Yogesh Malaiya <yogeshmalaiya2@gmail.com>
2025-11-19 09:22:58 +0530

    add pdfs and change in class.js

--- class.js
+++ class.js
@@ -17,5 +17,31 @@ class CreateUser {
         };
     }
 
+    write(text){
+        let h2 = document.createElement('h2');
+        h2.textContent = text;
+        document.body.appendChild(h2);
+
+    }
+
 }
+
+class AdminUser extends CreateUser {
+    constructor(name, email, username, password, pic, adminLevel) {
+        super(name, email, username, password, pic);
+        this.adminLevel = adminLevel;
+    }
+
+    remove(){
+        document.querySelectorAll("h2").forEach(h2 => h2.remove());
+    }
+    removeUser(username){
+        this.remove(username);
+    }
+}
+
+
+
 let user1 = new CreateUser("John Doe", "johndoe@mail.com", "johndoe", "password123", "path/to/pic.jpg");
+let user2 = new CreateUser("kia", "kia@mail.com", "kia", "password123", "path/to/pic.jpg");
+let admin = new AdminUser("admin", "admin@mail.com", "admin1", "password123", "path/to/pic.jpg");
